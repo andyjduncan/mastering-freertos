1c6046e6965da937c5de76862313f0b0945ba367
Andy Duncan <andy@adjectivecolournoun.com>
2024-08-21 17:22:41 +0100

    4.7 - Idle task hook

--- main/mastering_freertos_main.c
+++ main/mastering_freertos_main.c
@@ -1,25 +1,19 @@
+#define CONFIG_FREERTOS_USE_IDLE_HOOK 1
+
 #include <stdio.h>
-#include <stdbool.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-volatile unsigned long mainDELAY_LOOP_COUNT = 1000000;
+/* Declare a variable that will be incremented by the hook function.  */
+volatile unsigned long ulIdleCycleCount = 0UL;
 
-void vContinuousProcessingTask(void *pvParameters) {
-    char *pcTaskName;
-    /*
-     * The string to print out is passed in via the parameter. Cast this to a
-     * character pointer.
-     */
-    pcTaskName = (char *) pvParameters;
-    /* As per most tasks, this task is implemented in an infinite loop. */
-    for (;;) {
-        /*
-         * Print out the name of this task. This task just does this repeatedly
-         * without ever blocking or delaying.
-         */
-        printf("%s running\n", pcTaskName);
-    }
+/*
+ * Idle hook functions MUST be called vApplicationIdleHook(), take no
+ * parameters, and return void.
+ */
+void vApplicationIdleHook( void ) {
+    /* This hook function does nothing but increment a counter. */
+    ulIdleCycleCount++;
 }
 
 void vTaskFunction(void *pvParameters) {
@@ -27,7 +21,7 @@ void vTaskFunction(void *pvParameters) {
 
     TickType_t lastWakeTime = xTaskGetTickCount();
 
-    const TickType_t loopDelay = pdMS_TO_TICKS(10);
+    const TickType_t loopDelay = pdMS_TO_TICKS(250);
     /*
      * The string to print out is passed in via the parameter. Cast this to a
      * character pointer.
@@ -36,7 +30,7 @@ void vTaskFunction(void *pvParameters) {
     /* As per most tasks, this task is implemented in an infinite loop. */
     for (;;) {
         /* Print out the name of this task. */
-        printf("%s running\n", pcTaskName);
+        printf("%s running, %lu idle cycles\n", pcTaskName, ulIdleCycleCount);
         /* Delay for a period. */
         vTaskDelayUntil(&lastWakeTime, loopDelay);
     }
@@ -46,32 +40,10 @@ void app_main() {
 
     printf("Starting Task 1\n");
     xTaskCreatePinnedToCore(
-            vContinuousProcessingTask,
+            vTaskFunction,
             "Task 1",
             2000,
             (void *) "Task 1",
-            1,
-            NULL,
-            PRO_CPU_NUM
-    );
-
-    printf("Starting Task 2\n");
-    xTaskCreatePinnedToCore(
-            vContinuousProcessingTask,
-            "Task 2",
-            2000,
-            (void *) "Task 2",
-            1,
-            NULL,
-            PRO_CPU_NUM
-    );
-
-    printf("Starting Task 3\n");
-    xTaskCreatePinnedToCore(
-            vTaskFunction,
-            "Task 3",
-            2000,
-            (void *) "Task 3",
             2,
             NULL,
             PRO_CPU_NUM
