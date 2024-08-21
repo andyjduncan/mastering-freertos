aa27a0107a300ec4add399bf09117fe41162c1b1
Andy Duncan <andy@adjectivecolournoun.com>
2024-08-21 17:12:57 +0100

    4.6 - Mixing blocking and contiuous tasks

--- main/mastering_freertos_main.c
+++ main/mastering_freertos_main.c
@@ -5,12 +5,29 @@
 
 volatile unsigned long mainDELAY_LOOP_COUNT = 1000000;
 
+void vContinuousProcessingTask(void *pvParameters) {
+    char *pcTaskName;
+    /*
+     * The string to print out is passed in via the parameter. Cast this to a
+     * character pointer.
+     */
+    pcTaskName = (char *) pvParameters;
+    /* As per most tasks, this task is implemented in an infinite loop. */
+    for (;;) {
+        /*
+         * Print out the name of this task. This task just does this repeatedly
+         * without ever blocking or delaying.
+         */
+        printf("%s running\n", pcTaskName);
+    }
+}
+
 void vTaskFunction(void *pvParameters) {
     char *pcTaskName;
 
     TickType_t lastWakeTime = xTaskGetTickCount();
 
-    const TickType_t loopDelay = pdMS_TO_TICKS(250);
+    const TickType_t loopDelay = pdMS_TO_TICKS(10);
     /*
      * The string to print out is passed in via the parameter. Cast this to a
      * character pointer.
@@ -29,7 +46,7 @@ void app_main() {
 
     printf("Starting Task 1\n");
     xTaskCreatePinnedToCore(
-            vTaskFunction,
+            vContinuousProcessingTask,
             "Task 1",
             2000,
             (void *) "Task 1",
@@ -40,10 +57,21 @@ void app_main() {
 
     printf("Starting Task 2\n");
     xTaskCreatePinnedToCore(
-            vTaskFunction,
+            vContinuousProcessingTask,
             "Task 2",
             2000,
             (void *) "Task 2",
+            1,
+            NULL,
+            PRO_CPU_NUM
+    );
+
+    printf("Starting Task 3\n");
+    xTaskCreatePinnedToCore(
+            vTaskFunction,
+            "Task 3",
+            2000,
+            (void *) "Task 3",
             2,
             NULL,
             PRO_CPU_NUM
