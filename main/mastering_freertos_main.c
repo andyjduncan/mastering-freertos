009df58be9e25aac47c569a59489b2c2c7604d1a
Andy Duncan <andy@adjectivecolournoun.com>
2024-08-21 23:05:35 +0100

    6.1 - Some timers

--- main/mastering_freertos_main.c
+++ main/mastering_freertos_main.c
@@ -1,94 +1,73 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/timers.h"
 
-/* Declare two variables of type QueueHandle_t. Both queues are added
-   to the same queue set. */
-static QueueHandle_t xQueue1 = NULL, xQueue2 = NULL;
-/* Declare a variable of type QueueSetHandle_t. This is the queue set
-   to which the two queues are added. */
-static QueueSetHandle_t xQueueSet = NULL;
+/* The periods assigned to the one-shot and auto-reload timers are 3.333
+   second and half a second respectively. */
+#define mainONE_SHOT_TIMER_PERIOD pdMS_TO_TICKS( 3333 )
+#define mainAUTO_RELOAD_TIMER_PERIOD pdMS_TO_TICKS( 500 )
 
-void vSenderTask1(void *pvParameters) {
-    const TickType_t xBlockTime = pdMS_TO_TICKS(100);
-    const char *const pcMessage = "Message from vSenderTask1\n";
-    /* As per most tasks, this task is implemented within an infinite loop. */
-    while (true) {
-        /* Block for 100ms. */
-        vTaskDelay(xBlockTime);
-        /* Send this task's string to xQueue1. It is not necessary to use a
-           block time, even though the queue can only hold one item. This is
-           because the priority of the task that reads from the queue is
-           higher than the priority of this task; as soon as this task writes
-           to the queue it will be pre-empted by the task that reads from the
-           queue, so the queue will already be empty again by the time the
-           call to xQueueSend() returns. The block time is set to 0. */
-        xQueueSend(xQueue1, &pcMessage, 0);
-    }
+static void prvOneShotTimerCallback( TimerHandle_t xTimer ) {
+    TickType_t xTimeNow;
+    /* Obtain the current tick count. */
+    xTimeNow = xTaskGetTickCount();
+    /* Output a string to show the time at which the callback was executed. */
+    printf( "One-shot timer callback executing %lu\n", xTimeNow );
+    /* File scope variable. */
 }
 
-void vSenderTask2( void *pvParameters ) {
-    const TickType_t xBlockTime = pdMS_TO_TICKS( 200 );
-    const char * const pcMessage = "Message from vSenderTask2\n";
-    /* As per most tasks, this task is implemented within an infinite loop. */
-    while (true) {
-        /* Block for 200ms. */
-        vTaskDelay( xBlockTime );
-        /* Send this task's string to xQueue2. It is not necessary to use a
-           block time, even though the queue can only hold one item. This is
-           because the priority of the task that reads from the queue is
-           higher than the priority of this task; as soon as this task writes
-           to the queue it will be pre-empted by the task that reads from the
-           queue, so the queue will already be empty again by the time the
-           call to xQueueSend() returns. The block time is set to 0. */
-        xQueueSend( xQueue2, &pcMessage, 0 );
-    }
-}
-
-void vReceiverTask( void *pvParameters ) {
-    QueueHandle_t xQueueThatContainsData; char *pcReceivedString;
-    /* As per most tasks, this task is implemented within an infinite loop. */
-    while (true) {
-        /* Block on the queue set to wait for one of the queues in the set to
-           contain data. Cast the QueueSetMemberHandle_t value returned from
-           xQueueSelectFromSet() to a QueueHandle_t, as it is known all the
-           members of the set are queues (the queue set does not contain any
-           semaphores). */
-        xQueueThatContainsData = ( QueueHandle_t ) xQueueSelectFromSet(
-                xQueueSet, portMAX_DELAY );
-        /* An indefinite block time was used when reading from the queue set,
-           so xQueueSelectFromSet() will not have returned unless one of the
-           queues in the set contained data, and xQueueThatContainsData cannot
-           be NULL. Read from the queue. It is not necessary to specify a
-           block time because it is known the queue contains data. The block
-           time is set to 0. */
-        xQueueReceive( xQueueThatContainsData, &pcReceivedString, 0 );
-        /* Print the string received from the queue. */
-        printf( "%s", pcReceivedString );
-    }
+static void prvAutoReloadTimerCallback( TimerHandle_t xTimer ) {
+    TickType_t xTimeNow;
+    /* Obtain the current tick count. */
+    xTimeNow = xTaskGetTickCount();
+    /* Output a string to show the time at which the callback was executed. */
+    printf( "Auto-reload timer callback executing %lu\n", xTimeNow);
 }
 
 void app_main() {
 
-    /* Create the two queues, both of which send character pointers. The
-       priority of the receiving task is above the priority of the sending
-       tasks, so the queues will never have more than one item in them at
-       any one time*/
-    xQueue1 = xQueueCreate( 1, sizeof( char * ) ); xQueue2 = xQueueCreate( 1, sizeof( char * ) );
-    /* Create the queue set. Two queues will be added to the set, each of
-       which can contain 1 item, so the maximum number of queue handles the
-       queue set will ever have to hold at one time is 2 (2 queues multiplied
-       by 1 item per queue). */
-    xQueueSet = xQueueCreateSet( 1 * 2 );
+    TimerHandle_t xAutoReloadTimer, xOneShotTimer;
+    BaseType_t xTimer1Started, xTimer2Started;
 
-    /* Add the two queues to the set. */
-    xQueueAddToSet( xQueue1, xQueueSet );
-    xQueueAddToSet( xQueue2, xQueueSet );
-    /* Create the tasks that send to the queues. */
-    xTaskCreate( vSenderTask1, "Sender1", 2000, NULL, 1, NULL );
-    xTaskCreate( vSenderTask2, "Sender2", 2000, NULL, 1, NULL );
-    /* Create the task that reads from the queue set to determine which of
-       the two queues contain data. */
-    xTaskCreate( vReceiverTask, "Receiver", 2000, NULL, 2, NULL );
+    xOneShotTimer = xTimerCreate(
+            /* Text name for the software timer - not used by FreeRTOS. */
+            "OneShot",
+            /* The software timer's period in ticks. */
+            mainONE_SHOT_TIMER_PERIOD,
+            /* Setting uxAutoRealod to pdFALSE creates a one-shot software timer. */
+            pdFALSE,
+            /* This example does not use the timer id. */
+            0,
+            /* Callback function to be used by the software timer being created. */
+            prvOneShotTimerCallback );
+/* Create the auto-reload timer, storing the handle to the created timer
+   in xAutoReloadTimer. */
+    xAutoReloadTimer = xTimerCreate(
+            /* Text name for the software timer - not used by FreeRTOS. */
+            "AutoReload",
+            /* The software timer's period in ticks. */
+            mainAUTO_RELOAD_TIMER_PERIOD,
+            /* Setting uxAutoRealod to pdTRUE creates an auto-reload timer. */
+            pdTRUE,
+            /* This example does not use the timer id. */
+            0,
+            /* Callback function to be used by the software timer being created. */
+            prvAutoReloadTimerCallback );
 
+    if( ( xOneShotTimer != NULL ) && ( xAutoReloadTimer != NULL ) ) {
+        /* Start the software timers, using a block time of 0 (no block time).
+           The scheduler has not been started yet so any block time specified
+           here would be ignored anyway. */
+        xTimer1Started = xTimerStart(xOneShotTimer, 0);
+        xTimer2Started = xTimerStart(xAutoReloadTimer, 0);
+
+        if ((xTimer1Started == pdPASS) && (xTimer2Started == pdPASS)) {
+            printf("Started timers\n");
+        } else {
+            printf("Timers not started\n");
+        }
+    } else {
+        printf("Timers not created\n");
+    }
 }
