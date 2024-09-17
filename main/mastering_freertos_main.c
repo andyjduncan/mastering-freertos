a48280161fc5b1d7b30f43f133b0dda0c463f858
Andy Duncan <andy@adjectivecolournoun.com>
2024-09-17 18:53:33 +0100

    7.3 - Centralised deferred processing

--- main/mastering_freertos_main.c
+++ main/mastering_freertos_main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include "driver/gpio.h"
 #include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
 #include "freertos/portable.h"
 
-SemaphoreHandle_t  xCountingSemaphore;
+void vDeferredHandlingFunction(void *pvParameter1, uint32_t ulParameter2) {
+    printf( "Handler task - Processing event %lu.\n", ulParameter2);
+}
 
 static void isrHandler() {
+    static uint32_t ulParameterValue = 0;
 
     BaseType_t xHigherPriorityTaskWoken;
 
@@ -15,10 +17,16 @@ static void isrHandler() {
        API function if a context switch is required. */
     xHigherPriorityTaskWoken = pdFALSE;
 
-    /* 'Give' the semaphore to unblock the task, passing in the address of
-       xHigherPriorityTaskWoken as the interrupt safe API function's
-       pxHigherPriorityTaskWoken parameter. */
-    xSemaphoreGiveFromISR(xCountingSemaphore, &xHigherPriorityTaskWoken );
+    /* Send a pointer to the interrupt's deferred handling function to the
+       daemon task. The deferred handling function's pvParameter1 parameter
+       is not used so just set to NULL. The deferred handling function's
+       ulParameter2 parameter is used to pass a number that is incremented by
+       one each time this interrupt handler executes. */
+    xTimerPendFunctionCallFromISR( vDeferredHandlingFunction, /* Function to execute */
+                                   NULL, /* Not used */
+                                   ulParameterValue, /* Incrementing value. */
+                                   &xHigherPriorityTaskWoken );
+    ulParameterValue++;
 
     /* Pass the xHigherPriorityTaskWoken value into portYIELD_FROM_ISR().
       If xHigherPriorityTaskWoken was set to pdTRUE inside
@@ -28,32 +36,6 @@ static void isrHandler() {
       ports, the Windows port requires the ISR to return a value - the return
       statement is inside the Windows version of portYIELD_FROM_ISR(). */
     portYIELD_FROM_ISR( xHigherPriorityTaskWoken );
-}
-
-static void vHandlerTask( void *pvParameters ) {
-    /* xMaxExpectedBlockTime holds the maximum time expected between two
-       interrupts. */
-    const TickType_t xMaxExpectedBlockTime = pdMS_TO_TICKS(2000);
-
-    const TickType_t processingTime = pdMS_TO_TICKS(500);
-    /* As per most tasks, this task is implemented within an infinite loop. */
-    while(true) {
-        printf("Waiting for semaphore...\n");
-        /* Use the semaphore to wait for the event. The semaphore was created
-           before the scheduler was started, so before this task ran for the
-           first time. The task blocks indefinitely, meaning this function
-           call will only return once the semaphore has been successfully
-           obtained - so there is no need to check the value returned by
-           xSemaphoreTake(). */
-        if (xSemaphoreTake(xCountingSemaphore, xMaxExpectedBlockTime ) == pdPASS) {
-            /* To get here the event must have occurred. Process the event (in
-               this Case, just print out a message). */
-            printf( "Handler task - Processing event.\n" );
-            vTaskDelay(processingTime);
-        } else {
-            printf("Handler task - Timeout waiting for event.\n");
-        }
-    }
 }
 
 void app_main() {
@@ -76,20 +58,6 @@ void app_main() {
 
     gpio_isr_handler_add(GPIO_NUM_6, isrHandler, NULL);
 
-    /* Before a semaphore is used it must be explicitly created. In this
-       example a binary semaphore is created. */
-    xCountingSemaphore = xSemaphoreCreateCounting(5, 0);
-
-    /* Check the semaphore was created successfully. */
-    if(xCountingSemaphore != NULL ) {
-        /* Create the 'handler' task, which is the task to which interrupt
-           processing is deferred. This is the task that will be synchronized
-           with the interrupt. The handler task is created with a high priority
-           to ensure it runs immediately after the interrupt exits. In this
-           case a priority of 3 is chosen. */
-        xTaskCreate( vHandlerTask, "Handler", 4000, NULL, 3, NULL );
-
-        gpio_set_intr_type(GPIO_NUM_6, GPIO_INTR_LOW_LEVEL);
-        printf("Interrupt enabled\n");
-    }
+    gpio_set_intr_type(GPIO_NUM_6, GPIO_INTR_LOW_LEVEL);
+    printf("Interrupt enabled\n");
 }
