63cbd2cbdf840d62a0c21dd6a74269767b63cfa8
Andy Duncan <andy@adjectivecolournoun.com>
2024-10-27 10:23:12 +0000

    9.2 - Synchronising tasks

--- main/mastering_freertos_main.c
+++ main/mastering_freertos_main.c
@@ -1,4 +1,3 @@
-#include <sys/dirent.h>
 #include <sys/cdefs.h>
 #include <stdio.h>
 #include "driver/gpio.h"
@@ -6,100 +5,63 @@
 #include "freertos/portable.h"
 
 /* Definitions for the event bits in the event group. */
-#define mainFIRST_TASK_BIT ( 1UL << 0UL )   /* Event bit 0, set by a task */
-#define mainSECOND_TASK_BIT ( 1UL << 1UL )  /* Event bit 1, set by a task */
-#define mainISR_BIT ( 1UL << 2UL )          /* Event bit 2, set by an ISR */
+#define mainFIRST_TASK_BIT ( 1UL << 0UL ) /* Event bit 0, set by the 1st task */
+#define mainSECOND_TASK_BIT ( 1UL << 1UL ) /* Event bit 1, set by the 2nd task */
+#define mainTHIRD_TASK_BIT ( 1UL << 2UL ) /* Event bit 2, set by the 3rd task */
 
 EventGroupHandle_t  xEventGroup;
 
-_Noreturn static void vEventBitSettingTask( void *pvParameters ) {
-    const TickType_t xDelay200ms = pdMS_TO_TICKS( 200UL );
+_Noreturn static void vSyncingTask( void *pvParameters ) {
+    const TickType_t xMaxDelay = pdMS_TO_TICKS( 4000UL );
+    const TickType_t xMinDelay = pdMS_TO_TICKS( 200UL );
+    TickType_t xDelayTime;
+    EventBits_t uxThisTasksSyncBit;
+    const EventBits_t uxAllSyncBits = ( mainFIRST_TASK_BIT | mainSECOND_TASK_BIT | mainTHIRD_TASK_BIT );
+    /* Three instances of this task are created - each task uses a different
+       event bit in the synchronization. The event bit to use is passed into
+       each task instance using the task parameter. Store it in the
+       uxThisTasksSyncBit variable. */
+    uxThisTasksSyncBit = ( EventBits_t ) pvParameters;
     while(true) {
-        /* Delay for a short while before starting the next loop. */
-        vTaskDelay( xDelay200ms );
-        /* Print out a message to say event bit 0 is about to be set by the
-           task, then set event bit 0. */
-        printf( "Bit setting task -\t about to set bit 0.\n" );
-        xEventGroupSetBits( xEventGroup, mainFIRST_TASK_BIT );
-        /* Delay for a short while before setting the other bit. */
-        vTaskDelay( xDelay200ms );
-        /* Print out a message to say event bit 1 is about to be set by the
-           task, then set event bit 1. */
-        printf( "Bit setting task -\t about to set bit 1.\n" );
-        xEventGroupSetBits( xEventGroup, mainSECOND_TASK_BIT );
-    }
-}
-
-void vPrintStringFromDaemonTask(void * stringToPrint, uint32_t numParam) {
-    printf("%s", (char *)stringToPrint);
-}
-
-void vApplicationTickHook( void ) {
-    static const char *pcString = "Bit setting ISR -\t about to set bit 2.\n";
-    static int iCount = 0;
-    /* Print out a message every 500 ms. The message is not written out
-       directly, but sent to the gatekeeper task. */
-    iCount++;
-    if( iCount >= pdMS_TO_TICKS(500) ) {
-        BaseType_t xHigherPriorityTaskWoken = pdFALSE;
-        /* Print out a message to say bit 2 is about to be set. Messages cannot
-            be printed from an ISR, so defer the actual output to the RTOS daemon
-            task by pending a function call to run in the context of the RTOS
-            daemon task. */
-        xTimerPendFunctionCallFromISR( vPrintStringFromDaemonTask, ( void * ) pcString,
-                                       0,
-                                       &xHigherPriorityTaskWoken );
-
-        /* Set bit 2 in the event group. */
-        xEventGroupSetBitsFromISR( xEventGroup,
-                                   mainISR_BIT,
-                                   &xHigherPriorityTaskWoken );
-        /* Reset the count */
-        iCount = 0;
-    }
-}
-
-_Noreturn static void vEventBitReadingTask( void *pvParameters ) {
-    EventBits_t xEventGroupValue;
-    const EventBits_t xBitsToWaitFor = (mainFIRST_TASK_BIT |
-                                        mainSECOND_TASK_BIT |
-                                        mainISR_BIT);
-    while (true) {
-/* Block to wait for event bits to become set within the event
-   group. */
-        xEventGroupValue = xEventGroupWaitBits(
-                /* The event group to read */
+        /* Simulate this task taking some time to perform an action by delaying
+           for a pseudo random time. This prevents all three instances of this
+           task reaching the synchronization point at the same time, and so
+           allows the example's behavior to be observed more easily. */
+        xDelayTime = ( rand() % xMaxDelay ) + xMinDelay;
+        vTaskDelay( xDelayTime );
+        /* Print out a message to show this task has reached its synchronization
+           point. pcTaskGetTaskName() is an API function that returns the name
+           assigned to the task when the task was created. */
+        printf("%lu: %s reached sync point\n", xTaskGetTickCount(), pcTaskGetName(NULL));
+        /* Wait for all the tasks to have reached their respective
+           synchronization points. */
+        xEventGroupSync( /* The event group used to synchronize. */
                 xEventGroup,
-                /* Bits to test */
-                xBitsToWaitFor,
-                /* Clear bits on exit if the
-                   unblock condition is met */
-                pdTRUE,
-                /* Don't wait for all bits. This
-                   parameter is set to pdTRUE for
-                   second execution. */
-                pdFALSE,
-                /* Don't time out. */
-                portMAX_DELAY);
-/* Print a message for each bit that was set. */
-        if ((xEventGroupValue & mainFIRST_TASK_BIT) != 0) {
-            printf("Bit reading task -\t Event bit 0 was set\n");
-        }
-        if ((xEventGroupValue & mainSECOND_TASK_BIT) != 0) {
-            printf("Bit reading task -\t Event bit 1 was set\n");
-        }
-        if ((xEventGroupValue & mainISR_BIT) != 0) {
-            printf("Bit reading task -\t Event bit 2 was set\n");
-        }
+                /* The bit set by this task to indicate it has reached
+                   the synchronization point. */
+                uxThisTasksSyncBit,
+                /* The bits to wait for, one bit for each task taking
+                   part in the synchronization. */
+                uxAllSyncBits,
+                /* Wait indefinitely for all three tasks to reach the
+                   synchronization point. */
+                portMAX_DELAY );
+        /* Print out a message to show this task has passed its synchronization
+           point. As an indefinite delay was used the following line will only
+           be executed after all the tasks reached their respective
+           synchronization points. */
+        printf("%lu: %s exited sync point\n", xTaskGetTickCount(), pcTaskGetName(NULL));
     }
 }
 
 void app_main() {
-    /* Before an event group can be used it must first be created. */
+/* Before an event group can be used it must first be created. */
     xEventGroup = xEventGroupCreate();
-    /* Create the task that sets event bits in the event group. */
-    xTaskCreate( vEventBitSettingTask, "Bit Setter", 1000, NULL, 1, NULL );
-    /* Create the task that waits for event bits to get set in the event
-       group. */
-    xTaskCreate( vEventBitReadingTask, "Bit Reader", 1000, NULL, 2, NULL );
+    /* Create three instances of the task. Each task is given a different
+       name, which is later printed out to give a visual indication of which
+       task is executing. The event bit to use when the task reaches its
+       synchronization point is passed into the task using the task parameter. */
+    xTaskCreate(vSyncingTask, "Task 1", 2000, (void *) mainFIRST_TASK_BIT, 1, NULL );
+    xTaskCreate(vSyncingTask, "Task 2", 2000, (void *) mainSECOND_TASK_BIT, 1, NULL );
+    xTaskCreate(vSyncingTask, "Task 3", 2000, (void *) mainTHIRD_TASK_BIT, 1, NULL );
 }
